7c90f46c80176af03d56010eebb43494797ad53a
Abhishek Banyal <abhishekbanyal35@gmail.com>
2026-01-27 23:18:29 +0530

    Update cars.js

--- cars.js
+++ cars.js
@@ -10,7 +10,7 @@ const cars = [
 //- Find cars priced above ₹10,00,000
 let a =[]
 for ( let i; i<cars.length; i++){
-    if(cars[i].price<1000000){
+    if(cars[i].price>1000000){
         a.push[cars[a]]
     }
 }
