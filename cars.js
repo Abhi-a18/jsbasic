10c28bc828304cdf60bca12b50643fcc01811414
Abhishek Banyal <abhishekbanyal35@gmail.com>
2026-01-28 02:53:26 +0000

    Objects & Arrays of Objects (Cars)

--- cars.js
+++ cars.js
@@ -8,20 +8,55 @@ const cars = [
   { brand: "Mahindra", model: "XUV300", year: 2019, price: 900000 }
 ];
 //- Find cars priced above ₹10,00,000
-let a =[]
-for ( let i=0; i<cars.length; i++){
-    if(cars[i].price>1000000){
-        a.push(cars[i])
-    }
-}
-console.log(a)
-//  Find the oldest car
-let oldest = cars[0]
-for ( let i=0; i<cars.length; i++){
-    if(cars.year< oldest.year){
-        oldest = cars[i]
+// let a =[]
+// for ( let i=0; i<cars.length; i++){
+//     if(cars[i].price>1000000){
+//         a.push(cars[i])
+//     }
+// }
+// console.log(a)
+// //  Find the oldest car
+// let oldest = cars[0]
+// for ( let i=0; i<cars.length; i++){
+//     if(cars.year< oldest.year){
+//         oldest = cars[i]
            
-    }
+//     }
   
+// }
+//    console.log(oldest)
+// Find the average price
+let sum = 0
+for ( let i=0; i<cars.length; i++){
+    sum+= cars[i].price
 }
-   console.log(oldest)
+let r = sum/cars.length
+console.log(r)
+//Sort by price (low → high)
+const low = [...cars].sort((a, b) => a.price - b.price);
+console.log(low)
+//Sort by year (newest first)
+const high = [...cars].sort((a, b) => b.year - a.year);
+console.log(high)
+// Add a new car
+console.log(cars.push({brand: "Suzuki", model: "800", year: 2010, price: 80000}))
+console.log(cars)
+// Count cars manufactured after 2018
+let add =[]
+for(let i=0 ; i<cars.length; i++){
+    if(cars[i].year>2018){
+        add.push(cars[i])
+    }
+}
+   console.log(add)
+   // Find the most expensive car
+   let exp = []
+   let prices = cars[0]
+   for(let i =0; i< cars.length; i++){
+    for(let j =0 ; j< prices.length ; j++){
+    if(cars[i].price> prices[j].price){
+        exp.push(cars[i])
+    }
+    }
+   }
+   console.log(exp)
